bc44b0c7e834c89e85a009be963ae38a14bd149e
jangkyungdo <rudeh0926@naver.com>
2020-07-15 22:21:06 +0900

    Updates

---
+++ precache-manifest.2f57aa506c1db596d67e96a6ca525c38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e600a00dc4ac6af6814f66c5a64cd191",
+    "url": "/todolist-react/index.html"
+  },
+  {
+    "revision": "6ebe703b3d0db7153f92",
+    "url": "/todolist-react/static/css/main.c3fc0a48.chunk.css"
+  },
+  {
+    "revision": "c7b8b5f862bf7ce4484c",
+    "url": "/todolist-react/static/js/2.df740664.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist-react/static/js/2.df740664.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ebe703b3d0db7153f92",
+    "url": "/todolist-react/static/js/main.86fda56e.chunk.js"
+  },
+  {
+    "revision": "ab98de4c2a593e565d58",
+    "url": "/todolist-react/static/js/runtime-main.aab36768.js"
+  }
+]);
